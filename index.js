96f20066717c42766b170d0e991d7a6f5800e4e9
alaasao <a_souchi@estin.dz>
2024-07-03 19:15:33 +0100

    you

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
-const express = require("express")
-require("express-async-errors")
-const{app,server}=require("./socket/socket")
+const express = require("express");
+require("express-async-errors");
+const { app, server } = require("./socket/socket");
 
-require("dotenv/config")
-const cors = require("cors")
-const connectDb = require("./db/connect")
-const notFound = require("./middleware/not-found")
-const errorHandlerMiddleware =require("./middleware/error-handler")
-const authRouter = require("./routes/auth")
-const cookieParser = require("cookie-parser")
-app.use(
-    cors({
-      origin:process.env.DEPLOYED_URL,
-      preflightContinue: true,
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true,
-    })
-  );
+require("dotenv/config");
+const cors = require("cors");
+const connectDb = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const authRouter = require("./routes/auth");
+const cookieParser = require("cookie-parser");
+app.use(cors());
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-const userRouter=require("./routes/user")
-app.use("/auth", authRouter)
-app.use("/", userRouter)
-app.post("/jj", (req,res) => {
-    res.json({name:"ll"})
-})
-app.use(notFound)
-app.use(errorHandlerMiddleware)
-const port = process.env.PORT || 3000
+const userRouter = require("./routes/user");
+app.use("/auth", authRouter);
+app.use("/", userRouter);
+app.post("/jj", (req, res) => {
+  res.json({ name: "ll" });
+});
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
-const start = async() => {
-    try {
-        await connectDb(process.env.MONGO_URI)
-        server.listen(port, () => {
-            console.log("app is apping")
-        })
-    } catch (err) {
-        console.log(err)
-    }
-}
-start()
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    server.listen(port, () => {
+      console.log("app is apping");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
