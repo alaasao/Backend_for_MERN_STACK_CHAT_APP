ebba1131ad09774bddcf55718ef68b52bbc945fa
alaasao <a_souchi@estin.dz>
2024-07-03 15:43:59 +0100

    cors3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const notFound = require("./middleware/not-found")
 const errorHandlerMiddleware =require("./middleware/error-handler")
 const authRouter = require("./routes/auth")
 const cookieParser = require("cookie-parser")
+const cors=require("cors")
 app.use(
     cors({
       origins: [process.env.LOCAL_URL,process.env.DEPLOYED_URL],
